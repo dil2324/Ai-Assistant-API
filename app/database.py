077de96a18d7ae43e0d882d27a8df8a51cfa0d16
dild123 <di0230518@gmail.com>
2026-08-04 16:11:52 +0300

    add import psycopg

--- app/database.py
+++ app/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import  sessionmaker, DeclarativeBase
 from app.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+print("DATABASE_URL=", DATABASE_URL)
+engine = create_engine(DATABASE_URL,module=__import__("psycopg"))
 
 SessionLocal = sessionmaker(
     autoflush = False,
